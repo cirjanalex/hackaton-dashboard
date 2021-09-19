5387f3a47d32164d2a555f1d00f0f1e7b735f33f
Alexandru Cirjan <cirjan@mds.microsoft.com>
2021-09-19 19:08:46 +0300

    added a server

---
+++ src/store/index.js
@@ -0,0 +1,29 @@
+// import dependency to handle HTTP request to our back end
+import axios from 'axios'
+import Vuex from 'vuex'
+
+export default new Vuex.Store({
+//to handle state
+state: {
+    teams: []
+},
+
+//to handle state
+getters: {},
+
+
+//to handle mutations
+mutations: {
+    setTeams(state, teams) {
+        state.teams = teams;
+    }
+},
+
+//to handle actions
+actions: {
+    async fetchTeams({commit}) {
+        var teams = await axios.get("https://stefanini-hackathon2021-api.herokuapp.com/api/dashboard/teams");
+        commit('setTeams', teams);
+    }
+}
+});
