a20e5d7116674c20ab135a3d1c9b11a7a67138df
acirjan-akela <acirjan-akela@kds.com>
2021-09-21 19:48:47 +0200

    created the table

---
+++ src/components/TeamsTable.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="table">
+    <h1>Crypto Hackathon 2021</h1>
+    <v-data-table
+      :headers="headers"
+      :items="teams"
+      :items-per-page="10"
+      :sort-by="['estimatedValue']"
+      :sort-desc="[true]"
+      class="elevation-1"
+    ></v-data-table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TeamsTable",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      headers: [
+        {
+          text: "Team Name",
+          align: "start",
+          //sortable: false,
+          value: "name",
+        },
+        { text: "Transactions", value: "transactions" },
+        { text: "Estimated Value - USDT", value: "estimatedValue" },        
+        { text: "BTC", value: "BTC" },
+        { text: "ETH", value: "ETH" },
+        { text: "LTC", value: "LTC" },
+        { text: "BNB", value: "BNB" },
+        { text: "XRP", value: "XRP" },
+        { text: "TRX", value: "TRX" },
+        { text: "TRX", value: "TRX" }
+      ],
+    };
+  },
+  computed: {
+    teams() {
+      return this.$store.state.teams;
+    },
+  },
+  created() {
+    this.$store.dispatch("fetchTeams");
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.table {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+}
+.v-data-table {
+  width: 80%;
+}
+h1 {
+  padding: 2em;
+  color: rgb(75, 75, 75);
+}
+</style>
